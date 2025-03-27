f42cbe769a9e186802fd6aa5b35aafc58e0026dd
Nizham Tanjung <nizhamt@gmail.com>
2025-03-27 10:18:23 +0700

    fix gateway service

--- gateway-service.js
+++ gateway-service.js
@@ -1,38 +1,21 @@
 const express = require("express");
-const axios = require("axios");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+
 const app = express();
-const port = 3000;
 
-// Proxy configurations for User Service and Order Service
-const userServiceURL = "http://localhost:3001"; // User Service URL
-const assetServiceURL = "http://localhost:3002"; // Asset Service URL
+const userServiceURL = "http://localhost:3001/users";
+const assetServiceURL = "http://localhost:3002/assets"; // Asset Service URL
 
-// Proxy for User Service
-app.use(
-  "/users",
-  createProxyMiddleware({
-    target: userServiceURL,
-    changeOrigin: true,
-    pathRewrite: {
-      "^/users": "", // Remove '/users' from the URL path before forwarding to the User Service
-    },
-  })
-);
-
-// Proxy for Asset Service
-app.use(
-  "/assets",
-  createProxyMiddleware({
-    target: assetServiceURL,
-    changeOrigin: true,
-    pathRewrite: {
-      "^/assets": "", // Remove '/assets' from the URL path before forwarding to the Asset Service
-    },
-  })
-);
-
-// Start the Gateway service
-app.listen(port, () => {
-  console.log(`API Gateway running on http://localhost:${port}`);
+const userProxy = createProxyMiddleware({
+  target: userServiceURL,
+  changeOrigin: true,
 });
+
+const assetProxy = createProxyMiddleware({
+  target: assetServiceURL,
+  changeOrigin: true,
+});
+
+app.use("/v1/users", userProxy);
+app.use("/v1/assets", assetProxy);
+app.listen(3000);
