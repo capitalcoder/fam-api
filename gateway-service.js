e4d88e420ea71526e5207f458aac7cab10d21d2e
Nizham Tanjung <nizhamt@gmail.com>
2025-03-27 10:35:32 +0700

    delete prior gateway sample and tidy up new gateway

--- gateway-service.js
+++ gateway-service.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
+const port = 3000;
 
 const userServiceURL = "http://localhost:3001/users";
 const assetServiceURL = "http://localhost:3002/assets"; // Asset Service URL
@@ -9,13 +10,21 @@ const assetServiceURL = "http://localhost:3002/assets"; // Asset Service URL
 const userProxy = createProxyMiddleware({
   target: userServiceURL,
   changeOrigin: true,
+  pathRewrite: {
+    "^/users": "", // Remove '/users' from the URL path before forwarding to the User Service
+  },
 });
 
 const assetProxy = createProxyMiddleware({
   target: assetServiceURL,
   changeOrigin: true,
+  pathRewrite: {
+    "^/assets": "", // Remove '/users' from the URL path before forwarding to the User Service
+  },
 });
 
 app.use("/v1/users", userProxy);
 app.use("/v1/assets", assetProxy);
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`API Gateway running on http://localhost:${port}`);
+});
